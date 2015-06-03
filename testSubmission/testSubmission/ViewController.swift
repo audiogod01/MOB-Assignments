0081d35e56c2f699582ac128f31c34ff5098202b
Charlie Chao <charlieckchao@CharlieMacBookAir.local>
2015-06-03 21:45:16 +0800

    created test submission project

---
+++ testSubmission/testSubmission/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testSubmission
+//
+//  Created by Charlie Chao on 3/6/15.
+//  Copyright (c) 2015 Charlie Chao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
